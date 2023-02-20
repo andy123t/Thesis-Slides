65f8873f586cb789c8f88d0070944f3249165770
andy123t <andy123t@163.com>
2023-02-20 19:32:35 +0800

    Update

--- compile-xelatex.bat
+++ compile-xelatex.bat
@@ -28,7 +28,7 @@ set BSTINPUTS=.//;%BSTINPUTS%
 @rem ->> Delete temporary file
 @rem ------------------------------------------------
 del /q *.aux *.bbl *.blg *.log *.out *.toc *.bcf *.xml *.synctex *.nlo *.nls *.bak *.ind *.idx *.ilg *.lof *.lot *.ent-x *.tmp *.ltx *.los *.lol *.loc *.listing *.gz *.userbak *.nav *.snm *.vrb *.synctex(busy)
-del /q *.nav *.snm *.vrb *.fls *.xdv *.fdb_latexmk
+del /q *.nav *.snm *.vrb *.fls *.xdv *.fdb_latexmk *.thm
 @rem ------------------------------------------------
 @rem ->> View compiled file
 @rem ------------------------------------------------
--- compile-xelatex.sh
+++ compile-xelatex.sh
@@ -10,6 +10,6 @@ echo "${FileName%.tex}"
 xelatex --synctex=-1 "$FileName"
 xelatex --synctex=-1 "$FileName"
 #clear aux files
-rm -r *.aux *.bbl *.blg *.log *.out *.toc *.bcf *.xml *.synctex *.nlo *.nls *.bak *.ind *.idx *.ilg *.lof *.lot *.ent-x *.tmp *.ltx *.los *.lol *.loc *.listing *.gz *.userbak *.nav *.snm *.vrb 
+rm -r *.aux *.bbl *.blg *.log *.out *.toc *.bcf *.xml *.synctex *.nlo *.nls *.bak *.ind *.idx *.ilg *.lof *.lot *.ent-x *.tmp *.ltx *.los *.lol *.loc *.listing *.gz *.userbak *.nav *.snm *.vrb *.thm
 done
 
