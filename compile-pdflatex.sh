c311cd1bf8c50009d0de61c745f8ab49e6c52770
andy123t <andy123t@163.com>
2023-10-29 16:52:11 +0800

    update

--- compile-pdflatex.sh
+++ compile-pdflatex.sh
@@ -9,5 +9,5 @@ echo "${FileName%.tex}"
 pdflatex --synctex=-1 "$FileName"
 pdflatex --synctex=-1 "$FileName"
 #clear aux files
-rm -r *.aux *.bbl *.blg *.log *.out *.toc *.bcf *.xml *.synctex *.nlo *.nls *.bak *.ind *.idx *.ilg *.lof *.lot *.ent-x *.tmp *.ltx *.los *.lol *.loc *.listing *.gz *.userbak *.nav *.snm *.vrb 
+rm -r *.aux *.bbl *.blg *.log *.out *.toc *.bcf *.xml *.synctex *.nlo *.nls *.bak *.ind *.idx *.ilg *.lof *.lot *.ent-x *.tmp *.ltx *.los *.lol *.loc *.listing *.gz *.userbak *.nav *.snm *.vrb *.thm 
 done
